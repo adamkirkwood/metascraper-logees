adc18fdfdf00327603809f0dbe71d8de9c65a150
adamkirkwood <hello@adamkirkwood.com>
2020-06-15 18:43:50 -0700

    Test for unavailability & parse quanity

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 'use strict'
 
+const regex = /unavailable/g;
+
 /**
  * A set of rules we want to declare under `metascraper-logees` namespace.
  *
 **/
 module.exports = () => {
   const rules = {
-    availability: ({ htmlDom: $, url }) => $('p.availability').text(),
-    quantity: ({ htmlDom: $, url }) => $('div.availability-only').text()
+    availability: ({ htmlDom: $, url }) => ($('p.availability').text().match(regex) === null),
+    quantity: ({ htmlDom: $, url }) => parseInt($('div.availability-only').text())
   }
   return rules
 }
