afa15a3fb68802dad3a4aa9641d8f5367741b644
adamkirkwood <hello@adamkirkwood.com>
2020-06-17 22:28:25 -0700

    test URLs

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 const regex = /unavailable/g;
 
+const REGEX_LOGEES_URL = /(https?:\/\/(.+?\.)?logees\.com(\/[A-Za-z0-9\-\._~:\/\?#\[\]@!$&'\(\)\*\+,;\=]*)?)/
+
+const isValidUrl = memoizeOne(({ url }) => REGEX_LOGEES_URL.test(url))
+
 /**
  * A set of rules we want to declare under `metascraper-logees` namespace.
  *
@@ -11,5 +15,8 @@ module.exports = () => {
     availability: ({ htmlDom: $, url }) => (($('p.availability').text().match(regex) === null) || false),
     quantity: ({ htmlDom: $, url }) => (parseInt($('div.availability-only').text()) || 0)
   }
+
+  rules.test = isValidUrl
+
   return rules
 }
